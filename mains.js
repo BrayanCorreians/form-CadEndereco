2b39c15c9ebacbf6153606e4d9b12bb56239dd13
Etore <correiametore@gmail.com>
2024-09-20 09:16:57 -0300

    Alteraçao no js

--- mains.js
+++ mains.js
@@ -3,17 +3,29 @@
 
 //Função para Limpar formulario
 const LimparFormulario = () =>{
-    document.getElementById('Logradouro').Value = '';
+    document.getElementById('logradouro').Value = '';
+    document.getElementById('bairro').Value = '';
+    document.getElementById('localidade').Value = '';
     document.getElementById('uf').Value = '';
-    document.getElementById('Rua').Value = '';
-    document.getElementById('CEP').Value = '';
-    document.getElementById('Localidade').Value = '';
 }
 
 const eNumero = (numero) => /^[0-9]=$/.test(numero);
 // length e uma propiedade que identifca a quantidade de caracteres dentro do argumento cep
 const cepValido = (cep) => cep.length == 8 &&
 eNumero (cep);
+
+//Função para preencher formulario como campos de API
+
+const preencherFormulario = (endereço) =>{
+    document.getElementById('Logradouro').Value = endereço.logradouro;
+    document.getElementById('bairro').Value = endereco.bairro;
+    document.getElementById('localidade').Value = endereco.localidade;
+    document.getElementById('uf').Value = endereco.uf;
+  
+
+    
+    
+}
 
 
 
